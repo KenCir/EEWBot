9a25f490e23a4a505725d4a1a8e58603a9b1ca4d
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-05-21 04:34:24 +0900

    更新間隔を1秒に

--- src/events/discord/ready.ts
+++ src/events/discord/ready.ts
@@ -4,17 +4,17 @@ import notifyEEWReport from '../../functions/notifyEEWReport';
 import EEWMonitor from '../../functions/KyoushinMonitor';
 
 export default (client: EEWBot) => {
-    client.user?.setActivity('/help');
-    client.logger.info(`Logged in as ${client.user?.tag as string}`);
+  client.user?.setActivity('/help');
+  client.logger.info(`Logged in as ${client.user?.tag as string}`);
 
-    void client.voicevoxClient.notify('テスト');
+  void client.voicevoxClient.notify('テスト');
 
-    setInterval(() => {
-        void EEWMonitor(client);
-        void notifyEEWReport(client);
-    }, 15000);
+  setInterval(() => {
+    void EEWMonitor(client);
+    void notifyEEWReport(client);
+  }, 1000);
 
-    setInterval(() => {
-        spawn('php', ['QuakeInfo.php']);
-    }, 15000);
+  setInterval(() => {
+    spawn('php', ['QuakeInfo.php']);
+  }, 1000);
 };
