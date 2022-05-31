a44a61d4a4a2a0f392ce3dd1979840e66f921f1d
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-05-31 21:11:01 +0900

    強震モニタのデータ取得間隔を5秒から1秒に修正

--- src/events/discord/ready.ts
+++ src/events/discord/ready.ts
@@ -10,7 +10,7 @@ export default (client: EEWBot) => {
   setInterval(() => {
     void EEWMonitor(client);
     void notifyEEWReport(client);
-  }, 5000);
+  }, 1000);
 
   // NHKのQuakeInfo 15秒遅延
   setInterval(() => {
