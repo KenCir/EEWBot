a518f9475807ebb404e0a8f474f057e2c5949c9b
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-06-05 16:27:21 +0900

    VCの入退室でBotも入退出するように

--- src/events/discord/voiceStateUpdate.ts
+++ src/events/discord/voiceStateUpdate.ts
@@ -1,8 +1,19 @@
-import { VoiceState } from 'discord.js';
+import { DiscordGatewayAdapterCreator } from '@discordjs/voice';
+import { VoiceChannel, VoiceState } from 'discord.js';
 import EEWBot from '../../EEWBot';
 
 export default (client: EEWBot, oldState: VoiceState, newState: VoiceState) => {
-  if (!newState.guild.me?.voice && client.database.hasVoiceStatus(newState.channelId as string)) {
-    client.database.removeVoiceStatus(newState.channelId as string);
+  // 参加してたVCの人数がBot除いて0人になったら退出処理
+  if (oldState.channelId === oldState.guild.me?.voice.channelId && oldState.channel !== null) {
+    if ((oldState.channel as VoiceChannel).members.filter(m => !m.user.bot).size < 1) {
+      const voicevoxClient = client.voicevoxClient.get(oldState.guild.id);
+      if (voicevoxClient) {
+        client.voicevoxClient.leave(oldState.guild.id);
+      }
+    }
+  }
+  // 対象のVCに誰かが参加したら入室処理
+  else if (newState.channelId === client.database.getVoiceStatus(newState.guild.id)?.channel_id && (newState.channel as VoiceChannel).members.filter(m => !m.user.bot).size > 0 && !client.voicevoxClient.get(newState.guild.id)) {
+    client.voicevoxClient.add(newState.guild.id, newState.channelId, newState.guild.voiceAdapterCreator as DiscordGatewayAdapterCreator);
   }
 };
