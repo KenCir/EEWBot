3a616379a4deb592b903cf7b9be832ad1dadbc4c
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-07-28 22:15:20 +0900

    P2P地震通知情報追加

--- src/utils/IntensityUtil.ts
+++ src/utils/IntensityUtil.ts
@@ -1,44 +1,74 @@
-export function intensityStringToNumber(intensity: string): number {
-  if (Number(intensity)) return Number(intensity);
-
+export function intensityStringToNumber(intensity: string | number): number {
   switch (intensity) {
+    case 10:
+      return 1;
+    case 20:
+      return 2;
+    case 30:
+      return 3;
+    case 40:
+      return 4;
+    case 45:
+    case 46:
     case '5弱':
     case '5-':
       return 5;
+    case 50:
     case '5強':
     case '5+':
       return 5.5;
+    case 55:
     case '6弱':
     case '6-':
       return 6;
+    case 60:
     case '6強':
     case '6+':
       return 6.5;
+    case 70:
+    case -1:
+      return 7;
     default:
-      return -1;
+      {
+        if (Number(intensity)) return Number(intensity);
+        else return -1;
+      }
   }
 }
 
 export function intensityNumberToString(intensity: number): string {
   switch (intensity) {
     case 1:
+    case 10:
       return '震度1';
     case 2:
+    case 20:
       return '震度2';
     case 3:
+    case 30:
       return '震度3';
     case 4:
+    case 40:
       return '震度4';
     case 5:
+    case 45:
       return '震度5弱';
     case 5.5:
+    case 50:
       return '震度5強';
     case 6:
+    case 55:
       return '震度6弱';
     case 6.5:
+    case 60:
       return '震度6強';
     case 7:
+    case 70:
       return '震度7';
+    case 46:
+      return '震度5弱以上と予想されるが震度情報が入っていない';
+    case -1:
+      return '震度情報なし';
     default:
       // 通常なら絶対ない
       return 'Unknown';
--- src/utils/TunamiUtil.ts
+++ src/utils/TunamiUtil.ts
@@ -1,11 +1,31 @@
 export function tunamiEnumToString(type: string): string {
   switch (type) {
+    case 'None':
+      return 'なし';
+    case 'Checking':
+      return '調査中';
+    case 'NonEffective':
+      return '若干の海面変動が予想されるが、被害の心配なし';
     case 'Watch':
       return '津波注意報';
     case 'Warning':
       return '津波警報';
     case 'MajorWarning':
       return '大津波警報';
+    case 'NonEffectiveNearby':
+      return '震源の近傍で小さな津波の可能性があるが、被害の心配なし';
+    case 'WarningNearby':
+      return '震源の近傍で津波の可能性がある';
+    case 'WarningPacific':
+      return '太平洋で津波の可能性がある';
+    case 'WarningPacificWide':
+      return '太平洋の広域で津波の可能性がある';
+    case 'WarningIndian':
+      return 'インド洋で津波の可能性がある';
+    case 'WarningIndianWide':
+      return 'インド洋の広域で津波の可能性がある';
+    case 'Potential':
+      return '一般にこの規模では津波の可能性がある';
     case 'Unknown':
     default:
       return '不明';
